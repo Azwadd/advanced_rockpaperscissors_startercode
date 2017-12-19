9eca5491270de00d93015953eb03db9be4f497b3
Azwad <azwad.shameem0@gmail.com>
2017-12-19 02:24:02 +0000

    Features

--- script.js
+++ script.js
@@ -19,13 +19,6 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
 //GLOBAL VARIABLES
-var choices = ["rock" , "paper" , "scissors"];
-var computerNumber = Math.floor(Math.random() * 3);
-var computerChoice = choices[computerNumber];
-var winner = "";
-console.log(computerChoice);
-console.log(winner);
-console.log(choices);
 //FUNCTIONS
 
 // DOCUMENT READY FUNCTION
@@ -34,44 +27,141 @@ $(document).ready(function() {
                 e.preventDefault();
                 var userName = $("#name").val();
                 console.log(userName);
-                $("#nameValue").text(userName);
-                console.log(userChoice, "user choice")
+                $("#nameValue").text(userName).append("'s Choice");
         });
         $("#submitValue").click(function(e){
                 e.preventDefault();
                 var userChoice = $("#userValue").val();
-                if (computerChoice === userChoice){
-                        $("#uchoices").text(userChoice);
+                var choices = ["rock" , "paper" , "scissors" , "lizard" , "spock"];
+                var computerNumber = Math.floor(Math.random() * 5);
+                var computerChoice = choices[computerNumber]; 
+                console.log(computerChoice);
+                console.log(choices);
+                $("#crock").css("background-color" , "#333333");
+                $("#cpaper").css("background-color" , "#333333");
+                $("#cscissors").css("background-color" , "#333333");
+                $("#urock").css("background-color" , "#333333");
+                $("#upaper").css("background-color" , "#333333");
+                $("#uscissors").css("background-color" , "#333333");
+                if (computerChoice === userChoice.toLowerCase()){
+                        $("#uchoices").text(userChoice.toLowerCase());
                         $("#cchoice").text(computerChoice);
                         $("#won").text("It's a tie");
-                } else if (computerChoice === "rock" && userChoice === "scissors"){
-                        $("#uchoices").text("Scissors");
-                        $("#cchoice").text("Rock");
+                } else if (computerChoice === choices[0] && userChoice.toLowerCase() === choices[2]){
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Won!");
+                        $("#uresults").html(function(i, val) { return +val+1 ;});
+                        $("#crock").css("background-color" , "#daa520");
+                        $("#uscissors").css("background-color" , "#daa520");
+                } else if (computerChoice === choices[0] && userChoice.toLowerCase() === choices[1]){
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
                         $("#won").text("You Lost!");
-                } else if (computerChoice === "rock" && userChoice === "paper"){
-                        $("#uchoices").text("Paper");
-                        $("#cchoice").text("Rock");
-                        $("#won").text("You Won!");
-                } else if (computerChoice === "paper" && userChoice === "scissors"){
-                        $("#uchoices").text(userChoice);
+                        $('#cresults').html(function(i, val) { return +val+1 ;});
+                        $("#crock").css("background-color" , "#daa520");
+                        $("#upaper").css("background-color" , "#daa520");
+                } else if (computerChoice === choices[1] && userChoice.toLowerCase() === choices[0]){
+                        $("#uchoices").text(userChoice.toLowerCase());
                         $("#cchoice").text(computerChoice);
-                        $("#won").text("You Won!");
-                } else if (computerChoice === "paper" && userChoice === "rock"){
-                        $("#uchoices").text(userChoice);
+                        $("#won").text("You Lost!");
+                        $("#cresults").html(function(i, val) { return +val+1 ;});
+                        $("#urock").css("background-color" , "#daa520");
+                        $("#cpaper").css("background-color" , "#daa520");
+                } else if (computerChoice === choices[1] && userChoice.toLowerCase() === choices[2]){
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Won");
+                        $("#uresults").html(function(i, val) { return +val+1 ;});
+                        $("#uscissors").css("background-color" , "#daa520");
+                        $("#cpaper").css("background-color" , "#daa520");
+                } else if (computerChoice === choices[2] && userChoice.toLowerCase() === choices[0]){
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Won");
+                        $("#uresults").html(function(i, val) { return +val+1 ;});
+                        $("#urock").css("background-color" , "#daa520");
+                        $("#cscissors").css("background-color" , "#daa520");
+                } else if (computerChoice === choices[2] && userChoice.toLowerCase() ===  choices[1]){
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Lost!");
+                        $("#cresults").html(function(i, val) { return +val+1 ;});
+                        $("#upaper").css("background-color" , "#daa520");
+                        $("#cscissors").css("background-color" , "#daa520");
+                } else if (computerChoice === choices[3] && userChoice.toLowerCase() === choices [4]) {
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Lost!");
+                        $("#cresults").html(function(i, val) { return +val+1 ;});
+                } else if (computerChoice === choices[3] && userChoice.toLowerCase() === choices [1]) {
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Lost!");
+                        $("#cresults").html(function(i, val) { return +val+1 ;});
+                } else if (computerChoice === choices[4] && userChoice.toLowerCase() === choices [3]) {
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Lost!");
+                        $("#cresults").html(function(i, val) { return +val+1 ;});
+                } else if (computerChoice === choices[4] && userChoice.toLowerCase() === choices [1]) {
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Lost!");
+                        $("#cresults").html(function(i, val) { return +val+1 ;});
+                } else if (computerChoice === choices[3] && userChoice.toLowerCase() === choices[2]){
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Won");
+                        $("#uresults").html(function(i, val) { return +val+1 ;});
+                } else if (computerChoice === choices[3] && userChoice.toLowerCase() === choices[0]){
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Won");
+                        $("#uresults").html(function(i, val) { return +val+1 ;});
+                } else if (computerChoice === choices[4] && userChoice.toLowerCase() === choices[2]){
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Won");
+                        $("#uresults").html(function(i, val) { return +val+1 ;});
+                } else if (computerChoice === choices[4] && userChoice.toLowerCase() === choices[0]){
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Won");
+                        $("#uresults").html(function(i, val) { return +val+1 ;});
+                } else if (computerChoice === choices[0] && userChoice.toLowerCase() === choices[3]){
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Won");
+                        $("#uresults").html(function(i, val) { return +val+1 ;});
+                } else if (computerChoice === choices[2] && userChoice.toLowerCase() === choices[3]){
+                        $("#uchoices").text(userChoice.toLowerCase());
                         $("#cchoice").text(computerChoice);
                         $("#won").text("You Lost");
-                } else if (computerChoice === "scissors" && userChoice === "paper"){
-                        $("#uchoices").text(userChoice);
+                        $("#cresults").html(function(i, val) { return +val+1 ;});
+                } else if (computerChoice === choices[1] && userChoice.toLowerCase() === choices[3]){
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Won");
+                        $("#uresults").html(function(i, val) { return +val+1 ;});
+                } else if (computerChoice === choices[0] && userChoice.toLowerCase() === choices[4]){
+                        $("#uchoices").text(userChoice.toLowerCase());
+                        $("#cchoice").text(computerChoice);
+                        $("#won").text("You Won");
+                        $("#uresults").html(function(i, val) { return +val+1 ;});
+                } else if (computerChoice === choices[1] && userChoice.toLowerCase() === choices[4]){
+                        $("#uchoices").text(userChoice.toLowerCase());
                         $("#cchoice").text(computerChoice);
                         $("#won").text("You Lost");
-                } else if (computerChoice === "scissors" && userChoice === "rock"){
-                        $("#uchoices").text(userChoice);
+                        $("#uresults").html(function(i, val) { return +val+1 ;});
+                } else if (computerChoice === choices[2] && userChoice.toLowerCase() === choices[4]){
+                        $("#uchoices").text(userChoice.toLowerCase());
                         $("#cchoice").text(computerChoice);
-                        $("#won").text("You Won!");
+                        $("#won").text("You Won");
+                        $("#uresults").html(function(i, val) { return +val+1 ;});
                 } else {
+                        alert("Not Valid Input");
                         console.log("no conditions were true");
                 }
-                console.log(computerChoice, "computer choice");
-             
         });
 });
